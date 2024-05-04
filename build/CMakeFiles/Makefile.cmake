5515a7401f5707ce8c40c09044ec91ffaeed1b8b
j3n1xnoname <staspoliakov0@gmail.com>
2024-05-04 21:05:02 +0300

    Changed CMakeLists.txt a little

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -47,5 +47,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/CMakeStarterCpp.dir/DependInfo.cmake"
+  "CMakeFiles/Pokemon.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/Pokemon.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pokemon.dir/src/Attack.cpp.o"
+  "CMakeFiles/Pokemon.dir/src/Attack.cpp.o.d"
+  "CMakeFiles/Pokemon.dir/src/LTexture.cpp.o"
+  "CMakeFiles/Pokemon.dir/src/LTexture.cpp.o.d"
+  "CMakeFiles/Pokemon.dir/src/LTimer.cpp.o"
+  "CMakeFiles/Pokemon.dir/src/LTimer.cpp.o.d"
+  "CMakeFiles/Pokemon.dir/src/Player.cpp.o"
+  "CMakeFiles/Pokemon.dir/src/Player.cpp.o.d"
+  "CMakeFiles/Pokemon.dir/src/Pokemon.cpp.o"
+  "CMakeFiles/Pokemon.dir/src/Pokemon.cpp.o.d"
+  "CMakeFiles/Pokemon.dir/src/main.cpp.o"
+  "CMakeFiles/Pokemon.dir/src/main.cpp.o.d"
+  "Pokemon"
+  "Pokemon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pokemon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
